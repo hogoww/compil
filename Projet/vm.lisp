9e691819d091866c8a58553073195058223225af
Misse Pierre <pierre_misse25@msn.com>
2017-11-15 13:00:25 +0100

    start vm

--- Projet/vm.lisp
+++ Projet/vm.lisp
@@ -1,10 +1,33 @@
-(setf STACK_
+(setf STACK_SIZE 500)
+(setf MAIN_ADRESS 12) ;adress of the first instruction to evaluate
+(setf GETLINE 20) ;first line of compiled file must be the number of code line we have to load
 
-(setf VM (gensym))
-(print (symbol-plist VM))
-(setf (get VM 'R0) 0)
-(print (symbol-plist VM))
-(setf (get VM 'R1) '1)
-(print (symbol-plist VM))
-(setf (get VM 'R2) '2)
-(print (symbol-plist VM))
+
+(defun get-register (name symb)
+  (get name symb)
+)
+
+(defun make-vm (name)
+  (make-symbol name)
+  (setf (get name 'BP) (+ GETLINE 1))
+  (setf (get name 'SP) (symbol-plist 'BP))
+  (setf (get name 'MEM) (+ (get-register name 'BP) STACK_SIZE))
+  (setf (get name 'FP) nil)
+  
+  (setf (get name 'R0) 0)
+  (setf (get name 'R1) 0)
+  (setf (get name 'R2) 0)
+  (setf (get name 'RA) nil)
+  
+  (setf (get name 'FLG) nil)
+
+  (setf (get name 'PC) MAIN_ADRESS)
+)
+
+
+
+(setf VM 'beautiful-vm)
+(make-vm 'beautiful-vm)
+
+(print (symbol-plist 'VM))
+(print (get-register 'VM 'FLG))
