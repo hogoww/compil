2def67c8fe9bb481897b54318687a94489ee9cec
Misse Pierre <pierre_misse25@msn.com>
2017-12-27 12:52:15 +0100

    interface is good !

--- Projet/vm.lisp
+++ Projet/vm.lisp
@@ -35,12 +35,12 @@
     
     (setf (get symb 'set-addr)
 	  (lambda (addr value) )) ;Will be implemented at the same time as memory
-
+    
     (setf (get symb 'literalOrRegister)
 	  (lambda (v);if V is a literal, return the literal, otherwise return the register
 	    (if (integerp v)
 		v
-	      (get-register v)
+	      (funcall (get symb 'get-register) v)
 	      )))
 
     (setf (get symb 'VM) symb)
@@ -51,8 +51,8 @@
     (setf (get symb 'FP) nil)
     
     (setf (get symb 'R0) 0)
-    (setf (get symb 'R1) 0)
-    (setf (get symb 'R2) 0)
+    (setf (get symb 'R1) 12)
+    (setf (get symb 'R2) 34)
     (setf (get symb 'RA) nil)
     (setf (get symb 'FLG) nil)
     
@@ -63,105 +63,152 @@
     
     (setf (get symb 'MOVE)
 	  (lambda (target dest);targer register or integer. Dest register.
-	    (apply (get symb 'set-register) dest
-			  (literalOrRegister target))))
+	    (funcall (get symb 'set-register) 
+		   dest
+		   (funcall (get symb 'literalOrRegister) target))))
     
-    (setf (get symb 'LOAD) (lambda (addr R)
-      (apply (get symb 'set-register) R (get symb 'get-addr) addr)))
+    (setf (get symb 'LOAD) 
+	  (lambda (addr R)
+	    (funcall (get symb 'set-register)
+		     R 
+		     (get symb 'get-addr) addr)))
+  
+    (setf (get symb 'STORE) 
+	  (lambda  (reg addr)
+	    (funcall (get symb 'set-addr) 
+		     addr 
+		     (funcall (get symb 'get-register) reg))))
 
-    (setf (get symb 'STORE) (lambda  (reg addr)
-      (set-addr addr (get-register reg))))
-
-    (setf (get symb 'ADD) (lambda (R1 R2)
-      (apply (get symb 'set-register) R2
-		    (+ (get-register R1) (get-register R2)))))
-
-    (setf (get symb 'SUB) (lambda (R1 R2)
-      (setf (get-register R2) 
-	    (- (get-register R1) (get-register R2)))))
-
-    (setf (get symb 'MUL) (lambda (R1 R2)
-      (apply (get symb 'set-register) R2
-		    (* (get-register R1) (get-register R2)))))
-
-    (setf (get symb 'DIV) (lambda (R1 R2)
-      (apply (get symb 'set-register) R2
-		    (if (equal (get-register R1) 0)
-			(print "error div by 0")
-		      (/ (get-register R1) (get-register R2))))))
-
-    (setf (get symb 'PUSH) (lambda  (R value)
-      (setf (get-register SP) value)
-      (apply (get symb 'set-register) SP (+ (get-register SP) 1))))
-
-    (setf (get symb 'POP) (lambda (R)
-      (prog1 
-	  (get-register SP)
-	(apply (get symb 'set-register) SP (- (get-register SP) 1)))))
-
-    (setf (get symb 'CMP) (lambda (R1 R2); check if the order of the flag is ok
-      (if (equal (literalOrRegister R1) (get-register R2))
-	  (apply (get symb 'set-register) 'FLG DEQ)
-	(if (> (literalOrRegister R1) (get-register R2))
-	    (apply (get symb 'set-register) 'FLG DPG); R1 > R2
-	  (apply (get symb 'set-register) 'FLG DPP))))); R1 < R2 
-
-    (setf (get symb 'JPG) (lambda (etiq)
-      (if (equal (get-register 'FLG) DPG)
-	  (JMP etiq))))
-
-    (setf (get symb 'JEQ) (lambda (etiq)
-      (if (equal (get-register 'FLG) DEQ)
-	  (JMP etiq))))
+    (setf (get symb 'ADD) 
+	  (lambda (target dest)
+	    (funcall (get symb 'set-register) 
+		     dest
+		     (+ 
+		      (funcall (get symb 'literalOrRegister) target)
+		      (funcall (get symb 'get-register) dest)))))
     
-    (setf (get symb 'JPP ) (lambda (etiq)
-      (if (equal (get-register 'FLG) DPP)
-	  (JMP etiq))))
+    (setf (get symb 'SUB) 
+	  (lambda (target dest)
+	    (funcall (get symb 'get-register) dest) 
+		  (- (funcall (get symb 'literalOrRegister) target) (funcall (get symb 'get-register) dest))))
 
-    (setf (get symb 'JGE) (lambda (etiq)
-      (if (or (equal (get-register 'FLG) DPG) (equal (get-register 'FLG) DEQ))
-	  (JMP etiq))))
+    (setf (get symb 'MUL) 
+	  (lambda (target dest)
+	    (funcall (get symb 'set-register) 
+		     dest
+		     (* (funcall (get symb 'get-register) target) (funcall (get symb 'get-register) dest)))))
+    
+    (setf (get symb 'DIV) 
+	  (lambda (target dest)
+	    (funcall (get symb 'set-register) 
+	       dest
+	       (if (equal (funcall (get symb 'get-register) dest) 0)
+		   (warn "error div by 0")
+		 (/ (funcall (get symb 'get-register) target) (funcall (get symb 'get-register) dest))))))
+    
+    (setf (get symb 'PUSH) 
+	  (lambda  (R value)
+	    (progn (funcall (get symb 'set-addr)
+		     (funcall (get symb 'get-register) 'SP) value)
+	    (funcall (get symb 'set-register) 
+		     'SP 
+		     (+ (funcall (get symb 'get-register) 'SP) 1)))))
+   
+    (setf (get symb 'POP) 
+	  (lambda (R)
+	    (prog1 
+		(funcall (get symb 'get-register) 'SP)
+	      (funcall (get symb 'set-register) 
+		       'SP 
+		       (- (funcall (get symb 'get-register) 'SP) 1)))))
+    
+    (setf (get symb 'CMP)
+	  (lambda (R1 R2); check if the order of the flag is ok
+	    (if (equal (funcall (get symb 'get-register) R1) (funcall (get symb 'get-register) R2))
+		(funcall (get symb 'set-register) 'FLG DEQ);R1==R2
+	      (if (> (funcall (get symb 'get-register) R1) (funcall (get symb 'get-register) R2))
+		  (funcall (get symb 'set-register) 'FLG DPG); R1 > R2
+		(funcall (get symb 'set-register) 'FLG DPP))))); R1 < R2 
+    
+    (setf (get symb 'JPG) 
+	  (lambda (etiq)
+	    (if (equal (funcall (get symb 'get-register) 'FLG) DPG)
+		(funcall (get symb 'JMP) etiq))))
+    
+    (setf (get symb 'JEQ) 
+	  (lambda (etiq)
+	    (if (equal (funcall (get symb 'get-register) 'FLG) DEQ)
+		(funcall (get symb 'JMP) etiq))))
+    
+    (setf (get symb 'JPP ) 
+	  (lambda (etiq)
+	    (if (equal (funcall (get symb 'get-register) 'FLG) DPP)
+		(funcall (get symb 'JMP) etiq))))
 
-    (setf (get symb 'JPE) (lambda (etiq)
-      (if (or (equal (get-register 'FLG) DPE) (equal (get-register 'FLG) DEQ))
-	  (JMP etiq))))
+    (setf (get symb 'JGE) 
+	  (lambda (etiq)
+	    (if (or (equal (funcall (get symb 'get-register) 'FLG) DPG) (equal (funcall (get symb 'get-register) 'FLG) DEQ))
+		(funcall (get symb 'JMP) etiq))))
+    
+    (setf (get symb 'JPE) 
+	  (lambda (etiq)
+	    (if (or (equal (funcall (get symb 'get-register) 'FLG) DPE) (equal (funcall (get symb 'get-register) 'FLG) DEQ))
+		(funcall (get symb 'JMP) etiq))))
+    
+    (setf (get symb 'JMP) 
+	  (lambda (etiq)
+	    (funcall (get symb 'set-register)
+		     PC 
+		     etiq)))
 
-    (setf (get symb 'JMP) (lambda (etiq)
-      (apply (get symb 'set-register) PC etiq)))
+    (setf (get symb 'JSR) 
+	  (lambda (etiq)
+	    (funcall (get symb 'PUSH) (funcall (get symb 'get-register) RA));push
+	    (funcall (get symb 'JMP) etiq)))
 
-    (setf (get symb 'JSR) (lambda (etiq)
-      (p (get-register RA));push
-      (JMP etiq)))
+    (setf (get symb 'RTN)
+	  (lambda (etiq)
+	    (funcall (get symb 'POP) 
+		     (funcall (get symb 'get-addr) (funcall (get symb 'get-register) 'SP))
+		     'PC)))
+    
+    (setf (get symb 'NOP)
+	  (lambda ()));to do
 
-    (setf (get symb 'RTN) (lambda (etiq)
-      (JMP (p1 (get-register)))))
+    (setf (get symb 'HALT)
+	  (lambda ()));To do
 
-    (setf (get symb 'NOP) (lambda ()))
+    (setf (get symb 'CONS) 
+	  (lambda (v R2);cons ;;v can be a value or a register
+	    (funcall (get symb 'set-register) R2 
+		     (cons (funcall (get symb 'literalOrRegister) v))
+		     (funcall (get symb 'get-register) R2))))
 
-    (setf (get symb 'HALT) (lambda ()));To do
+    (setf (get symb 'CAR) 
+	  (lambda (target dest);car
+	    (funcall (get symb 'set-register) 
+		     dest
+		     (car (funcall (get symb 'get-register) target)))))
 
-    (setf (get symb 'CONS) (lambda (v R2);cons ;;v can be a value or a register
-      (apply (get symb 'set-register) R2 
-		    (cons (get-register (literalOrRegister v)) 
-			  (get-register R2)))))
-
-    (setf (get symb 'CAR) (lambda (R);car
-      (apply (get symb 'set-register) R (car (get-register R1)))))
-
-    (setf (get symb 'CDR) (lambda (R);cdr
-      (apply (get symb 'set-register) R (cdr (get-register R1)))))
-
+    (setf (get symb 'CDR) 
+	  (lambda (target dest);cdr
+	    (funcall (get symb 'set-register) 
+		     dest
+		     (cdr (funcall (get symb 'get-register) target)))))
     ))
 
 
 (setf vm (make-vm 'VM "VM0"))
 
-(defun func-vm (funcName arg)
-  (apply (get vm funcname) arg))
 
-(func-vm 'print-property '(R1))
-(func-vm 'set-register '(R1 12))
-(func-vm 'print-property '(R1))
-(func-vm 'get-register '(R1))
-(func-vm 'print-vm '())
-(func-vm 'MOVE '(R1 R2))
+;; (func-vm 'print-property '(R1))
+;; (func-vm 'set-register '(R1 12))
+;; (func-vm 'get-register '(R1))
+;;(func-vm 'print-vm '())
+
+;(funcall (get vm 'print-vm))
+(funcall (get vm 'print-property) 'R1)
+(funcall (get vm 'print-property) 'R2)
+(funcall (get vm 'MOVE) 'R2 'R1)
+(funcall (get vm 'print-property) 'R1)
+(funcall (get vm 'print-property) 'R2)
