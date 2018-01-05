3d89da25d8c32ae79d6459021ae32269214468c9
Misse Pierre <pierre_misse25@msn.com>
2018-01-05 23:47:30 +0100

    basic code enough for fibo, debug & change in VM needed

--- Projet/compil.lisp
+++ Projet/compil.lisp
@@ -31,74 +31,78 @@
 (defun next_label ()
   (setf label (+ label 1)))
 
+(defun compile-args (list-arg env)
+  (if (null list-arg)
+      nil
+      (append (step1 (car list-arg) env) 
+	      (list '(PUSH R0))
+	      (compile-args (cdr list-arg) env))))
+    
 
-(defun step (expr pos env)
+(defun make-env (list-arg nb-arg)
+  (if (eq 0 nb-arg)
+      nil
+    (cons (const (car list-arg) nb-arg) (make-env (cdr list-arg) (- nb-arg 1)))))
+  
+
+(defun step1 (expr env)
   (cond 
    ((atom expr) 
     (if (constantp expr)
-	(cons (cons 'MOVE expr) 'R0)	
+	(cons (cons (cons 'MOVE expr) 'R0) nil)
       (let ((a (list_assoc_search env expr)))
-	(if (a)
+	(if (not (null a))
 	    (list '(MOVE FP R0)
-		  (cons (cons 'SUB (cdr a)) 'R0);(cdr a) position dans la liste d'arguments de la func
+		  (list 'SUB (cadr a) 'R0);(cdr a) position dans la liste d'arguments de la func
 		  '(LOAD R0 R0))
 	  (error "~s Variable doesn't exist." expr)
 	  ))))
    ;; ((atom (car expr) ;si 
-   ((equal (car expr) 'if) (append (step (cadr expr) env)
-				   '(CMP R0 nil)
+   ((equal (car expr) 'if) (append (step1 (cadr expr) env)
+				   (list '(CMP R0 nil))
 				   (let ((else (next_label))
 					 (end (next_label)))
 				     
-				     (append 
-				      (if (cadddr expr)
-					  (cons 'JEQ else)
-					(cons 'JEQ end))
-				      (step (caddr expr) env)
+				     (append
+				      (list 
+				       (if (not (null (cadddr expr)))
+					   (cons 'JEQ else)
+					 (cons 'JEQ end)))
+				      (step1 (caddr expr) env)
 				      
-				      (if (cadddr expr)
-					  (append (cons 'JMP end)
-						  (cons 'label else)
-						  (step (cadddr expr) env)))
-				      (cons 'label end)))))
-   ((equal (car expr 'defun)) (if (fbounp (cadr expr))
+				      (if (not (null (cadddr expr)))
+					  (append (list (cons 'JMP end)
+						  (cons 'label else))
+						  (step1 (cadddr expr) env)))
+				      (list (cons 'label end))))))
+   ((equal (car expr) 'defun) (if (not (null (fbounp (cadr expr))))
 				  (error "You're trying to replace a lisp function")
-				(append (cons 'label (cadr expr))
-					(step (cadddr expr) (make-env (caddr expr)))
-					'(RTN))))
-
+				(append (list (cons 'label (cadr expr)))
+					(step1 (cadddr expr) (make-env (caddr expr)))
+					(list '(RTN)))))
+  
 					;Ajouter un cas QUOTE 
 
-   (t (append (compile-args (cdr expr) env)
-	      (cons (cons 'MOVE (- (length expr) 1)) 'R0)
-	      '(PUSH R0)
-	      '(MOVE FP R1)
-	      '(MOVE SP FP)
-	      '(MOVE SP R2)
-	      (cons (cons 'SUB (length expr)) 'R2);Enleve n+1 arguments
-	      '(PUSH R2)
-	      '(PUSH R1)
-	      (cons 'JSR (car expr))
-	      '(POP R1)
-	      '(POP R2)
-	      '(MOVE R1 FP)
-	      '(MOVE R2 SP)))
-		    
-	      
-				      
-   
+   (t (append (compile-args (cdr expr) env);funcall
+	      (list (list 'MOVE (- (length expr) 1) 'R0)
+		    '(PUSH R0)
+		    '(MOVE FP R1)
+		    '(MOVE SP FP)
+		    '(MOVE SP R2)
+		    (list 'SUB (length expr) 'R2);Enleve n+1 arguments
+		    '(PUSH R2)
+		    '(PUSH R1)
+		    (list 'JSR (car expr))
+		    '(POP R1)
+		    '(POP R2)
+		    '(MOVE R1 FP)
+		    '(MOVE R2 SP))))
+
    ))
 
-(defun compile-args (list-arg env)
-  (if (list-arg)
-      (append (step (car list-arg)) 
-	      '(PUSH R0)
-	      (compile-args (cdr arg) env))
-    nil))
 
 
-
-(print (step '(f 1) '((f 1))))
+(print (step1 '(+ f 2) (list '(f 1))))
 
 ;; ;(with-open-file (str "./file.lispvm"
 ;;                      :direction :output
