114a5f1b3f353ad24f2d68dde17f00c9ecb13336
Alisu <theorogliano@hotmail.com>
2018-01-07 11:35:31 +0100

    Correction d'une parenthèse fermante sur fibo.lisp // Ajout fibo.livm

--- Projet/fibo.lisp
+++ Projet/fibo.lisp
@@ -4,9 +4,9 @@
 ;;     (* n (fact (- n 1)))))
 
 (defun fibo (n)
-  (if (or (= n 0) (= n 1)
+  (if (or (= n 0) (= n 1))
       1
-      (+ (fibo (- n 1)) (fibo (- n 2))))))
+      (+ (fibo (- n 1)) (fibo (- n 2)))))
 
 ;;(fact 4)
 (fibo 4)
